93625dd9525cd4e434b39f81655cbe3f9b251407
Raman Singh <58785293+singhrama@users.noreply.github.com>
2022-08-22 10:54:17 -0400

    Update streamlit_app.py
    added pancake image and smoothie option

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
-streamlit.text("Pan Cakes with maple Syrup")
+streamlit.text("🥞 Pan Cakes with maple Syrup")
 streamlit.text("🥑🍞 Avocado Toast")
 
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
